ff72b0455ff0893853e8515ac2355c4017c38456
Wesley <jw.bodoso@gmail.com>
2022-02-08 22:01:05 +0800

    Search Bar

--- app/src/main/java/com/funtease/practice/MainActivity.kt
+++ app/src/main/java/com/funtease/practice/MainActivity.kt
@@ -1,45 +1,61 @@
 package com.funtease.practice
 
+import android.app.Activity
 import android.app.SearchManager
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import android.view.Menu
-import android.widget.SearchView
-import androidx.appcompat.content.res.AppCompatResources
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.funtease.practice.adapters.JobAdapter
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.funtease.practice.adapters.ViewPagerAdapter
 import com.funtease.practice.databinding.ActivityMainBinding
+import com.funtease.practice.fragments.HomeFragment
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewPagerAdapter: ViewPagerAdapter
+    private lateinit var homeFragment: HomeFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        stUpJobRecyclerView()
+        viewPagerAdapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+        homeFragment = HomeFragment()
+        viewPagerAdapter.addFragment(homeFragment, "HOME")
+        viewPagerAdapter.addFragment(HomeFragment(), "INBOX")
+
+        binding.pager.adapter = viewPagerAdapter
+
+        TabLayoutMediator(binding.tabLayout, binding.pager, true) { tab, position ->
+            tab.text = viewPagerAdapter.getTitle(position)
+            val badge = tab.orCreateBadge
+            badge.number = 99
+        }.attach()
+
+        binding.tabLayout.addOnTabSelectedListener(object : OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                tab.removeBadge()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {}
+            override fun onTabReselected(tab: TabLayout.Tab) {}
+        })
+
     }
 
-    private fun stUpJobRecyclerView() {
-        val jobList = ArrayList<String>()
-        jobList.add("Hello")
-        jobList.add("Hello")
-        jobList.add("Hello")
-        jobList.add("Hello")
-        val linearLayoutManager = LinearLayoutManager(this)
-        with(binding.sideList) {
-            layoutManager = linearLayoutManager
-            adapter = JobAdapter(jobList)
-        }
-    }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.options_menu, menu)
         val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
         if (menu != null) {
-            (menu.findItem(R.id.search).actionView as SearchView).apply {
+            (menu.findItem(R.id.menu_search).actionView as android.widget.SearchView).apply {
                 // Assumes current activity is the searchable activity
                 setSearchableInfo(searchManager.getSearchableInfo(componentName))
                 isIconifiedByDefault = false // Do not iconify the widget; expand it by default
@@ -48,4 +64,11 @@ class MainActivity : AppCompatActivity() {
 
         return true
     }
+
+    fun displaySpeechRecognizer() {
+
+
+    }
+
+
 }
