693a7895fc1377a7bd76888913abbc13b8c67dc9
Wesley <jw.bodoso@gmail.com>
2022-02-15 00:15:50 +0800

    Improve Home UI/UX

--- app/src/main/java/com/funtease/practice/MainActivity.kt
+++ app/src/main/java/com/funtease/practice/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.funtease.practice
 
+import android.app.Activity
 import android.app.SearchManager
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.speech.RecognizerIntent
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.Menu
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.funtease.practice.adapters.ViewPagerAdapter
 import com.funtease.practice.databinding.ActivityMainBinding
 import com.funtease.practice.fragments.HomeFragment
+import com.funtease.practice.fragments.ProfileFragment
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
 import com.google.android.material.tabs.TabLayoutMediator
@@ -17,6 +29,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewPagerAdapter: ViewPagerAdapter
     private lateinit var homeFragment: HomeFragment
+    private lateinit var searchText: EditText
+    private lateinit var launchSomeActivity: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +41,7 @@ class MainActivity : AppCompatActivity() {
         homeFragment = HomeFragment()
         viewPagerAdapter.addFragment(homeFragment, "HOME")
         viewPagerAdapter.addFragment(HomeFragment(), "INBOX")
-        viewPagerAdapter.addFragment(HomeFragment(), "PROFILE")
+        viewPagerAdapter.addFragment(ProfileFragment(), "PROFILE")
 
         binding.pager.adapter = viewPagerAdapter
         binding.pager.isUserInputEnabled = false
@@ -46,6 +60,55 @@ class MainActivity : AppCompatActivity() {
             override fun onTabReselected(tab: TabLayout.Tab) {}
         })
 
+        launchSomeActivity =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val data: Intent? = result.data
+                    val spokenText: String? =
+                        data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                            .let { results ->
+                                results!![0]
+                            }
+                    searchText.setText(spokenText)
+                    //TODO : Search
+                }
+            }
+
+        setUpSearchBar()
+    }
+
+    private fun setUpSearchBar() {
+        val searchBar: View = binding.root.findViewById(R.id.search_layout)
+        val voice = searchBar.findViewById<ImageView>(R.id.search_voice)
+        val close = searchBar.findViewById<ImageView>(R.id.search_close)
+        searchText = searchBar.findViewById(R.id.search_text)
+
+        close.setOnClickListener {
+            Handler().postDelayed({
+                searchText.text.clear()
+            }, 180)
+        }
+
+        voice.setOnClickListener {
+
+            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+                putExtra(
+                    RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                    RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+                )
+            }
+            // This starts the activity and populates the intent with the speech text.
+            launchSomeActivity.launch(intent)
+        }
+        searchText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                voice.visibility = if (s.isEmpty()) View.VISIBLE else View.GONE
+                close.visibility = if (s.isNotEmpty()) View.VISIBLE else View.GONE
+            }
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
--- app/src/main/java/com/funtease/practice/fragments/HomeFragment.kt
+++ app/src/main/java/com/funtease/practice/fragments/HomeFragment.kt
@@ -1,107 +1,39 @@
 package com.funtease.practice.fragments
 
-import android.app.Activity
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.speech.RecognizerIntent
-import android.text.Editable
-import android.text.TextWatcher
 import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
-import android.widget.ImageView
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearSnapHelper
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.SnapHelper
-import com.funtease.practice.MainActivity
-import com.funtease.practice.R
 import com.funtease.practice.adapters.JobAdapter
 import com.funtease.practice.adapters.JobAdapterExplore
 import com.funtease.practice.databinding.FragmentHomeBinding
-import com.funtease.practice.views.CenterZoomLinearLayoutManager
+import com.funtease.practice.utils.CenterZoomLinearLayoutManager
 
 
 class HomeFragment : Fragment() {
 
     lateinit var binding: FragmentHomeBinding
-    private lateinit var mainActivity: MainActivity
-    private lateinit var launchSomeActivity: ActivityResultLauncher<Intent>
-    private lateinit var searchText: EditText
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?) : View {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        launchSomeActivity =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-                if (result.resultCode == Activity.RESULT_OK) {
-                    val data: Intent? = result.data
-                    val spokenText: String? =
-                        data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
-                            .let { results ->
-                                results!![0]
-                            }
-                    searchText.setText(spokenText)
-                    //TODO : Search
-                }
-            }
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        mainActivity = activity as MainActivity
-        setUpSearchBar()
         setUpRecyclerView()
     }
 
-    private fun setUpSearchBar() {
-        val searchBar: View = binding.root.findViewById(R.id.search_layout)
-        val voice = searchBar.findViewById<ImageView>(R.id.search_voice)
-        val close = searchBar.findViewById<ImageView>(R.id.search_close)
-        searchText = searchBar.findViewById(R.id.search_text)
-
-        close.setOnClickListener {
-            Handler().postDelayed({
-                searchText.text.clear()
-            }, 180)
-        }
-
-        voice.setOnClickListener {
-
-            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
-                putExtra(
-                    RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                    RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
-                )
-            }
-            // This starts the activity and populates the intent with the speech text.
-            launchSomeActivity.launch(intent)
-        }
-        searchText.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable) {}
-            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
-
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                voice.visibility = if (s.isEmpty()) View.VISIBLE else View.GONE
-                close.visibility = if (s.isNotEmpty()) View.VISIBLE else View.GONE
-            }
-        })
-    }
 
     private fun setUpRecyclerView() {
         with(binding.jobList) {
-            layoutManager = CenterZoomLinearLayoutManager(activity!!.baseContext, 1f, 0.2f)
+            layoutManager = CenterZoomLinearLayoutManager(activity!!.baseContext, 1f, 0.175f)
             val jobList = ArrayList<String>()
             jobList.add("SAMPLE")
             jobList.add("SAMPLE")
@@ -113,7 +45,7 @@ class HomeFragment : Fragment() {
             val displayMetrics = DisplayMetrics()
             activity?.windowManager?.defaultDisplay?.getMetrics(displayMetrics)
             val width = displayMetrics.widthPixels
-            adapter = JobAdapter(jobList, width)
+            adapter = JobAdapter(jobList, width, activity!!)
             val helper: SnapHelper = LinearSnapHelper()
             helper.attachToRecyclerView(this)
             addOnScrollListener(object : RecyclerView.OnScrollListener() {
